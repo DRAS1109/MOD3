6a1ae49432e5af913d4884a0a8dac82f49fc0b26
DRAS1109 <25049@esenviseu.net>
2024-11-13 11:34:44 +0000

    13/11/2024

--- 01.py
+++ 01.py
@@ -1,25 +1,57 @@
+#Calculadora com Funções
+import math
+
+def Soma(X, Y):
+    """Função para somar X e Y e apresentar o resultado"""
+    print (f"{X} + {Y} = {X + Y}")
+
+def Subtração(X, Y):
+    """Função para subtrair X e Y e apresentar o resultado"""
+    print (f"{X} - {Y} = {X - Y}")
+
+def Multiplicação(X, Y):
+    """Função para multiplicar X e Y e apresentar o resultado"""
+    print (f"{X} * {Y} = {round ((X * Y), 3)}")
+
+def Divisão(X, Y):
+    """Função para dividir X e Y e apresentar o resultado"""
+    print (f"{X} / {Y} = {round ((X / Y), 3)}")
+
+
 def Menu():
     """
-    Mostra ao utilizados as operações que a calcuadora vai utilizar
+    Mostra ao utilizador as operações que a calcuadora vai utilizar
     """
-    Ação = input("Qual função pretende efetuar? \n + = Soma \n - = Subtração\n * = Multiplicação \n / = Divisão")
-    if Ação == "+":
-        Soma()
 
-    elif Ação == "-":
-        Subtração()
+    Ação = None
+    while Ação.upper().strip() != "T":
 
-    elif Ação == "*":
-        Multiplicação()
+        Ação = input("Qual função pretende efetuar? \n + = Soma \n - = Subtração\n * = Multiplicação \n / = Divisão \n T = Terminar \n")
+        if Ação == "5":
+            break
 
-    elif Ação == "/":
-        Divisão()
+        X = float(input(f"Introduza o 1º Nº: "))
+        Y = float(input(f"Introduza o 2º Nº: "))
 
-        
+        if Ação.strip() == "+":
+            #Ação_Extenso = "Somar"
+            Soma(X, Y)
+
+        elif Ação.strip() == "-":
+            #Ação_Extenso = "Subtrair"
+            Subtração(X, Y)
+
+        elif Ação.strip() == "*":
+            #Ação_Extenso = "Multiplicar"
+            Multiplicação(X, Y)
+
+        elif Ação.strip() == "/":
+            #Ação_Extenso = "Dividir"
+            Divisão(X, Y)
+
+
 def main():
     Menu()
-
-
 
 if __name__ == "__main__":
     main()
---
+++ Materia/Parametros.py
@@ -0,0 +1,16 @@
+def Somar (x, y):
+    print (x + y)
+    x = 0
+    y = 0
+
+def main():
+    Somar("Joaquim", "Maria")
+    Somar(10, 5)
+    Somar(10.3, -3.2)
+    n = 10
+    z = 15
+    Somar (n, z)
+    print (n, z)
+
+if __name__ == '__main__':
+   main() 
